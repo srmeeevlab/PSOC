cbddf06bcc2f15cba7c758835ff9cec550bf1a96
Raman-Singh-Shekhawat <rs8893@srmist.edu.in>
2022-07-29 00:10:40 +0530

    REVAMPEDREVAMPED

--- 7_State_estimation_using_weighted_least_square_7/round-template/experiment/simulation/js/main.js
+++ 7_State_estimation_using_weighted_least_square_7/round-template/experiment/simulation/js/main.js
@@ -300,13 +300,10 @@ const deleteRowShunt = ()=>{
     updateResistanceMatrix("dec");
 };
 const getValue = (id)=>{
+    // console.log("id is ",id)
     const el = document.getElementById(id);
-    
+    // console.log(el)
     const val = el.value;
-    if(!val || val=="" || isNaN(val)){
-        alert("enter the value. you have left the field empty");
-        // return
-    }
     if(Number(val) == val){
         const valNum = Number(val);
         const min = Number(el.getAttribute("min"));
@@ -341,8 +338,14 @@ const makeAPMatrix = ()=>{
     const length = el.querySelectorAll("tr").length;
     const arr = [];
     for(i=1; i<=length; i++){
-        arr.push([_c(getValue(`ap-sb-${i}`)),_c(getValue(`ap-rb-${i}`)),_c(getValue(`ap-p-${i}`)),_c(getValue(`ap-r-${i}`))]);
+        arr.push([
+            _c(getValue(`ap-sb-${i}`)),
+            _c(getValue(`ap-rb-${i}`)),
+            _c(getValue(`ap-p-${i}`)),
+            _c(getValue(`ap-r-${i}`))
+        ]);
     }
+    // console.log("pdata returning is ",arr)
     return arr;
 }
 const makeAPIMatrix = ()=>{
@@ -350,7 +353,11 @@ const makeAPIMatrix = ()=>{
     const length = el.querySelectorAll("tr").length;
     const arr = [];
     for(i=1; i<=length; i++){
-        arr.push([_c(getValue(`api-b-${i}`)),_c(0),_c(getValue(`api-p-${i}`)),_c(getValue(`api-r-${i}`))]);
+        arr.push([
+            _c(getValue(`api-b-${i}`)),
+            _c(0),_c(getValue(`api-p-${i}`)),
+            _c(getValue(`api-r-${i}`))
+        ]);
     }
     return arr;
 }
@@ -391,15 +398,19 @@ const makeShuntMatrix = ()=>{
     return arr;
 }
 const makeResistanceMatrix = ()=>{
+    const el = document.getElementById("resistance-matrix");
     const res = [];
-    for(row = 0; row<length;row++){
-        const row =[];
-        for(column = 0; column<length;column++){
-            row.push(_c(getVal(`rm-${row}-${column}`)));
+    const length = el.querySelectorAll("input").length;
+    // console.log("length is",length)
+    for(let row = 0; row<math.sqrt(length);row++){
+        const row1 =[];
+        // console.log()
+        for(column = 0; column<math.sqrt(length);column++){
+            row1.push(_c(getValue(`rm-${row}-${column}`)));
         }
-        res.push(row);
+        res.push(row1);
     }
-    console.log(res);
+    // console.log(res);
     return res;
 }
 const _nbus = ()=>{
@@ -411,7 +422,7 @@ const fixMaxBus = ()=>{
         element = document.getElementsByClassName("bus").item(i);
         element.setAttribute("max", _nbus());
         element.setAttribute("min", 1);
-        console.log(element);
+        // console.log(element);
     };
 }
 const updateResistanceMatrix = (n)=>{
@@ -443,7 +454,6 @@ const buildResistanceMatrix = (length,el)=>{
     }
 }
 const runSIM = ()=>{
-    try{
     fixMaxBus();
     const busdata = makeBusMatrix();
     const pdata1 = makeAPMatrix();
@@ -453,6 +463,7 @@ const runSIM = ()=>{
     const vdata = makeVMatrix();
     const Ri = makeResistanceMatrix();
     const shunt_data = makeShuntMatrix();
+    // console.log("done with shunt")
     const npi = Number(pdata2.length);
     const nqi = Number(qdata2.length);
     const npf = Number(pdata1.length);
@@ -462,13 +473,43 @@ const runSIM = ()=>{
     let z = [];
     const n_bus = _nbus();
     const nbus = n_bus;
-    z = z.concat(pdata2.map(getLastCol),qdata2.map(getLastCol),pdata1.map(getLastCol),qdata1.map(getLastCol),vdata.map(getLastCol));
-    const del = math.zeros(nbus,1)._data;
-    let v = math.ones(nbus,1)._data;
+    // console.log("pdata",pdata1)
+    // console.log("pdata",pdata2)
+    // console.log("qdata",qdata1)
+    // console.log("qdata",qdata2)
+    // console.log("vdata",vdata)
+    // z = z.concat(
+    //     pdata2.map(getLastCol),
+    //     qdata2.map(getLastCol),
+    //     pdata1.map(getLastCol),
+    //     qdata1.map(getLastCol),
+    //     vdata.map(getLastCol)
+    // );
+    for (let i = 0; i < pdata1.length; i++) {
+        z.push(pdata1[i][2].re)
+    }
+    for (let i = 0; i < pdata2.length; i++) {
+        z.push(pdata2[i][2].re)
+    }
+    for (let i = 0; i < qdata1.length; i++) {
+        z.push(qdata1[i][2].re)
+    }
+    for (let i = 0; i < qdata2.length; i++) {
+        z.push(qdata2[i][2].re)
+    }
+    for (let i = 0; i < vdata.length; i++) {
+        z.push(vdata[i][2].re)
+    }
+
+
+    // console.log("z is",z)
+    const del = math.zeros(nbus)._data; //modified
+    let v = math.ones(nbus)._data; //modified
     let n =3 , iter = 1;
     const n_elements = busdata.length;
     const n_shunt = Number(shunt_data.length); 
     const ybus = math.zeros(n_bus,n_bus)._data;
+    // console.log("done with ybus")
     for (i = 0; i<n_elements;i++){
         let p = busdata[i][1].re;
         let q = busdata[i][2].re;
@@ -487,99 +528,267 @@ const runSIM = ()=>{
             yele = shunt_data[j][2];
             ybus[q][q] = math.add(ybus[q][q],yele);
         }
-    const E = [[del.slice(1)],[v]];
+    let E = [...del.slice(1),...v];
     const G = math.matrix(ybus).map((val)=>{return val.re})._data;
     const B = math.matrix(ybus).map((val)=>{return val.im})._data;
+    // console.log("E is",E)
+    // console.log("G is",G)
+    // console.log("B is",B)
     let tol = 5;
-   
-    runJ()}
-    catch (err){
-
-    }
-}
-
-function generateRandom(min = 0.9201, max = 0.9999) {
-
-    let difference = max - min;
-
-    let rand = Math.random();
-
-
-    rand =( rand * difference);
-
-    rand = rand + min;
-
-    return rand.toFixed(4);
-}
-
-const runJ = ()=>{
-    let nbus = document.getElementById("nbus").value
-    if(!nbus || isNaN(nbus) || nbus==""){
-        alert("enter the bus value")
-        return;
-    }
-    //  h m v del
-    const h = math.ones(2)._data;
-    const m=2;
-    const v = math.zeros(3)._data;
-    const del = math.zeros(3)._data;
-
-    v[0] = generateRandom();
-    v[1] = generateRandom();
-    v[2] = generateRandom();
-
-    del[1] = generateRandom(-0.001,0)
-    del[2] = generateRandom(0,0.03)
-
-    let outputs = document.getElementById("OUTPUT")
-    outputs.innerHTML=""
-    outputs.style.border = "0.25rem solid black"
-
-    let ans1 = document.createElement("div")
-    let ans2 = document.createElement("div")
-    let ans3 = document.createElement("div")
-    let ans4 = document.createElement("div")
-    let ans5 = document.createElement("div")
-    let ans6 = document.createElement("div")
-    let ans7 = document.createElement("div")
-    let ans8 = document.createElement("div")
-
-    // ans2.innerHTML = "At the mid-point,the voltage without compensation is:" + Vm.toFixed(2) + " V."
-    // ans3.innerHTML = 'Reactive power is required to maintain the mid-point voltage at 1 per unit ' +Qr.toFixed(2)+ ' var.'
+    // console.log("entering while ")
+    // while(tol>1e-4){
+        const h1 = math.zeros(nvi)._data;
+        const h2 = math.zeros(npi)._data;
+        const h3 = math.zeros(nqi)._data;
+        const h4 = math.zeros(npf)._data;
+        const h5 = math.zeros(nqf)._data;
+        for(i=0; i<nvi; i++){
+            h1[i] = v[i];
+        }
+        for(i=0;i<npi;i++){
+            const m = Number(pdata2[i][0].re) - 1;
+            for(k=0; k<nbus;k++){
+                // h2[i] = h2[i]+v[m]*v[k]*(G[m][k]*Math.cos(del[m]-del[k])) + B[m][k]*Math.sin(del[m] - del[k]);
+                
+                // console.log( "h2",typeof h2[i])
+                // console.log(typeof B[m][k])
+                // console.log(typeof G[m][k])
+                // console.log(typeof v[m])
+                h2[i] = h2[i] + v[m]*v[k]*(G[m][k]*Math.cos(del[m]-del[k]) + B[m][k]*Math.sin(del[m]-del[k]));
+            }
+        }
+        for(i=0;i<nqi;i++){
+            const m = qdata2[i][0].re - 1;
+            for(k=0; k<nbus;k++){
+                // h3[i] = h3[i]+v[m]*v[k]*(G[m][k]*Math.sin(del[m]-del[k])) - B[m][k]*Math.cos(del[m] - del[k]);
+                h3[i] = h3[i] + v[m]*v[k]*(G[m][k]*Math.sin(del[m]-del[k]) - B[m][k]*Math.cos(del[m]-del[k]));
+            }
+        }
+        for(i=0;i<npf;i++){
+            const m = pdata1[i][0].re - 1;
+            const n = pdata1[i][1].re - 1;
+            h4[i] = -v[m]*v[m]*G[m][n] - v[m]*v[n]*(-G[m][n]*Math.cos(del[m]-del[n]) - B[m][n]*Math.sin(del[m]-del[n]));
+        }
+        for(i=0;i<nqf;i++){
+            const m = qdata1[i][0].re - 1;
+            const n = qdata1[i][1].re - 1;
+            h5[i] = -v[m]*v[m]*(-B[m][n]) - v[m]*v[n]*(-G[m][n]*Math.sin(del[m]-del[n]) + B[m][n]*Math.cos(del[m]-del[n]));
+        }
+        // console.log("done with h")
+        let h = [...h4,...h2,...h5,...h3,...h1];
+        
     
-    ans1.innerHTML = `h = ${h[0]}  ${h[1]} `
-    ans2.innerHTML = `m = ${m}`
-    ans3.innerHTML = `V1 = ${v[0]}`
-    ans4.innerHTML = `V2 = ${v[1]}`
-    ans5.innerHTML = `V3 = ${v[2]}`
-    ans6.innerHTML = `del1 = ${del[0]}`
-    ans7.innerHTML = `del2 = ${del[1]}`
-    ans8.innerHTML = `del3 = ${del[2]}`
+        console.log("h is",h)
+        console.log("z is ",z)
+        const r= math.subtract(math.matrix(z),math.matrix(h))._data;
+        console.log("r is",r)
+        // console.log('r is',r)
+        const H11 = math.zeros(nvi,nbus-1)._data;
+        const H12 = math.zeros(nvi,nbus)._data;
+        for(k=0; k<nvi;k++){
+            for(n=0;n<nbus;n++){
+                if(n==k){
+                    H12[k][n] = 1;
+                }
+            }
+        }
+        // console.log("H21")
+        const H21 = math.zeros(npi,nbus-1)._data;
+        for(i=0;i<npi;i++){
+            const m = pdata2[i][0].re - 1;
+            for(k=0; k<nbus-1;k++){
+                if(k+1 == m){
+                    for(n=0;n<nbus;n++){
+                        H21[i][k] = H21[i][k] + v[m]*v[n]*(-G[m][n]*Math.sin(del[m]-del[n]) + B[m][n]*Math.cos(del[m]-del[n]));
+                    }
+                    H21[i][k] = H21[i][k] - v[m]*v[m]*B[m][m];
+                }
+                else{
+                    H21[i][k] = v[m]*v[k+1]*(G[m][k+1]*Math.sin(del[m]-del[k+1]) - B[m][k+1]*Math.cos(del[m]-del[k+1]));
+                }
+            }
+        }
+        // console.log("H31")
+        const H22 = math.zeros(npi,nbus)._data;
+        for(i=0;i<npi;i++){
+            const m = pdata2[i][0].re - 1;
+            for(k=0; k<nbus;k++){
+                if(k == m){
+                    for(n=0;n<nbus;n++){
+                        H22[i][k] = H22[i][k] + v[n]*(G[m][n]*Math.cos(del[m]-del[n]) + B[m][n]*Math.sin(del[m]-del[n]));
+                    }
+                    H22[i][k] = H22[i][k] + v[m]*G[m][m];
+                }
+                else{
+                    H22[i][k] = v[m]*( G[m][k]*Math.cos(del[m]-del[k]) + B[m][k]*Math.sin(del[m]-del[k]))
+                }
+            }
+        }
+        // console.log("H22")
+        const H31 = math.zeros(nqi,nbus-1)._data;
+        for(i=0;i<nqi;i++){
+            const m = qdata2[i][0].re-1;
+            for(k=0;k<nbus-1;k++){
+                if(k+1 == m){
+                    for(n=0;n<nbus;n++){
+                        H31[i][k] = H31[i][k] + v[m]*v[n]*(G[m][n]*Math.cos(del[m]-del[n]) + B[m][n]*Math.sin(del[m]-del[n]));
+                    }
+                    H31[i][k] = H31[i][k] - v[m]*v[m]*G[m][m];
+                }
+                else{
+                    H31[i][k] = v[m]*v[k+1]*(-G[m][k+1]*Math.cos(del[m]-del[k+1]) - B[m][k+1]*Math.sin(del[m]-del[k+1]));
+                }
+            }
+        }
+        // console.log("H32")
+        const H32 = math.zeros(nqi,nbus)._data;
+        for(i=0;i<nqi;i++){
+            const m = qdata2[i][0].re-1;
+            for(k=0;k<nbus;k++){
+                if(k==m){
+                    for(n=0;n<nbus;n++){
+                        H32[i][k] = H32[i][k] + v[n]*(G[m][n]*Math.sin(del[m]-del[n]) - B[m][n]*Math.cos(del[m]-del[n]));
+                    }
+                    H32[i][k] = H32[i][k] - v[m]*B[m][m];
+                }
+                else{
+                    H32[i][k] = v[m]*(G[m][k]*Math.sin(del[m]-del[k]) - B[m][k]*Math.cos(del[m]-del[k]));
+                }
+            }
+        }
+        // console.log("H41")
+        const H41 = math.zeros(npf,nbus-1)._data;
+        for(i=0;i<npf;i++){
+            const m = pdata1[i][0].re-1;
+            const n = pdata1[i][1].re-1;
+            for(k=0;k<nbus-1;k++){
+                if(k+1 == m){
+                    H41[i][k] = v[m]*v[n]*(-G[m][n]*Math.sin(del[m]-del[n]) + B[m][n]*Math.cos(del[m]-del[n]));
+                }
+                else {
+                    if(k+1 == n){
+                        H41[i][k] = -v[m]*v[n]*(-G[m][n]*Math.sin(del[m]-del[n]) + B[m][n]*Math.cos(del[m]-del[n]));
+                    }
+                    else{
+                        H41[i][k] = 0;
+                    }
+                }
+            }
+        }
+        // console.log("H42")
+        const H42 = math.zeros(npf,nbus)._data;
+        for(i=0;i<npf;i++){
+            const m = pdata1[i][0].re-1;
+            const n = pdata1[i][1].re-1;
+            for(k=0;k<nbus;k++){
+                if(k == m){
+                    H42[i][k] = -v[n]*(-G[m][n]*Math.cos(del[m]-del[n]) - B[m][n]*Math.sin(del[m]-del[n])) - 2*G[m][n]*v[m];
+                }
+                else{
+                    if(k==n){
+                        H42[i][k] = -v[m]*(-G[m][n]*Math.cos(del[m]-del[n]) - B[m][n]*Math.sin(del[m]-del[n]));
+                    }
+                    else{
+                        H42[i][k] = 0;
+                    }
+                }
+            }
+        }
+        // console.log("H51")
+        const H51 = math.zeros(nqf, nbus-1)._data;
+        for(i=0;i<nqf;i++){
+            m = qdata1[i][0].re - 1;
+            n = qdata1[i][1].re - 1;
+            for(k=0;k<nbus-1;k++){
+                if(k+1 == m){
+                    H51[i][k] = -v[m]*v[n]*(-G[m][n]*Math.cos(del[m]-del[n]) - B[m][n]*Math.sin(del[m]-del[n]));
+                }
+                else{
+                    if(k+1 == n){
+                        H51[i][k] = v[m]*v[n]*(-G[m][n]*Math.cos(del[m],del[n]) - B[m][n]*Math.sin(del[m]-del[n]));
+                    }
+                    else{
+                        H51[i][k] = 0;
+                    }
+                }
+            }
+        }
+        // console.log("H52")
+        const H52 = math.zeros(nqf,nbus)._data;
+        for(i=0;i<nqf;i++){
+            const m = qdata1[i][0].re -1;
+            const n = qdata1[i][1].re -1;
+            for(k=0;k<nbus;k++){
+                if(k==m){
+                    H52[i][k] = -v[n]*(-G[m][n]*Math.sin(del[m]-del[n]) + B[m][n]*Math.cos(del[m] - del[n]));
+                }
+                else{
+                    if(k == n){
+                        H52[i][k] = -v[m]*(-G[m][n]*Math.sin(del[m]-del[n]) + B[m][n]*Math.cos(del[m]-del[n]));
+                    }
+                    else{
+                        H52[i][k] = 0;
+                    }
+                }
+            }
+        }
+        console.log("done wirh H")
+        // const H = [[H21.concat(H22)], [H31.concat(H32)],[H51.concat(H52)],[H41.concat(H42)],[H11.concat(H12)]];
+        // console.log("H22",H22)
+        const H = [
+            ...math.concat(H21, H22),
+            ...math.concat(H31, H32),
+            ...math.concat(H51, H52),
+            ...math.concat(H41, H42),
+            ...math.concat(H11, H12)
+        ]
+        console.log("H is")
+        console.log(H)
+        // console.log("h not")
+        console.log("ri ",Ri)
 
-    ans1.style.marginBottom = "1rem"
-    ans2.style.marginBottom = "1rem"
-    ans3.style.marginBottom = "2rem"
-    ans4.style.marginBottom = "2rem"
-    ans5.style.marginBottom = "2rem"
-    ans6.style.marginBottom = "2rem"
-    ans7.style.marginBottom = "2rem"
-    ans8.style.marginBottom = "2rem"
+        // ====================
 
-    let heading = document.createElement("div")
-    heading.innerHTML = "Results:"
-    heading.style.fontWeight = "900"
-    heading.style.marginBottom = "3rem"
-    heading.style.marginTop = "2rem"
-    heading.id = "result"
+        console.log("inv Ri")
+        console.log(math.inv(Ri))
 
-    outputs.appendChild(heading)
-    // outputs.appendChild(ans1)
-    // outputs.appendChild(ans2)
-    outputs.appendChild(ans3)
-    outputs.appendChild(ans4)
-    outputs.appendChild(ans5)
-    outputs.appendChild(ans6)
-    outputs.appendChild(ans7)
-    outputs.appendChild(ans8)
+        console.log("transpose H")
+        console.log(math.transpose(H))
+        let Gm = []
+        Gm = math.multiply(math.transpose(H),math.inv(Ri))
+        console.log("Gm half is",Gm)
 
+        Gm = math.multiply(Gm,H)
+        // let Gm1 = math.multiply(math.inv(Ri),H)
+        // let Gm2 = math.multiply(math.transpose(H),Gm1)
+        console.log("Gm2  is",Gm)
+
+        const J = math.sum(math.multiply(math.inv(Ri),r,r));
+
+        console.log("J is",J)
+
+        const dE = math.multiply(math.inv(Gm), math.multiply(math.transpose(H), math.inv(Ri),r));
+        
+        E = math.add(E,dE);
+
+        for(i=1, j=0;i<del.length;i++,j++){
+            del[i] = E[j];
+        }
+        // console.log("not v")
+        v = E.slice(nbus-1);
+        iter = iter+1;
+        tol = math.max(math.abs(dE));
+        
+        console.log("del",del)
+        
+        console.log("E",E)
+        console.log("v",v)
+        console.log("tol",tol)
+    // }
+    console.log("done with while as well")
+    // const CvE = math.diag(math.multiply(math.transpose(H),math.inv(Ri),H));
+    // const Del = math.multiply((180/Math.PI),del);
+    // const E2 = v.concat(Del);
+    console.log("v is",v);
+    console.log("del is",del);
 }
